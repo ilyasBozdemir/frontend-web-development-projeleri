4886f9c18bab8a8ce7e7ca4d9248ce48801da178
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-07-24 21:24:45 +0300

    updated

---
+++ note-taking-app/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles.module.css";
+
+function Header() {
+  return (
+    <>
+      <h1 className={styles.header}> React App Markdown Notes</h1>
+    </>
+  );
+}
+
+export default Header;
