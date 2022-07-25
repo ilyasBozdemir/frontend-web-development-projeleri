eb42a3dca797e6b9d2b68762644623191e91f059
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-07-25 10:38:27 +0300

    updated

--- chat-app/client/src/App.js
+++ chat-app/client/src/App.js
@@ -1,14 +1,25 @@
-import "./App.css";
-
-import { ChatProvider } from "./context/ChatContext";
-import Container from "./components/Container";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<ChatProvider>
-			<Container />
-		</ChatProvider>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
